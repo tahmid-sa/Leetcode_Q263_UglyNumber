f4d48c0a6c67ba84183490962fcb514cc830df49
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-07-09 01:53:57 -0400

    Adding files to github

---
+++ Q263_UglyNumber/Q263_UglyNumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+bool isUgly(int n) {
+    if (n == 0)
+        return false;
+
+    if (n == 1)
+        return true;
+
+    while (((n % 2) == 0) || ((n % 3) == 0) || ((n % 5) == 0)) {
+
+        if ((n % 2) == 0)
+            n /= 2;
+
+        if ((n % 3) == 0)
+            n /= 3;
+
+        if ((n % 5) == 0)
+            n /= 5;
+
+    }
+
+    if (n != 1)
+        return false;
+
+    return true;
+}
+
+int main() {
+    cout << isUgly(6);
+
+    return 0;
+}
